29bcb937e1597ed21c69cb5aaada9c3c22b6bceb
Altaha Ansari <altaha.altaha@gmail.com>
2020-06-22 18:48:37 +0530

    updated robotics projects info

--- Services/roboticsProjects.js
+++ Services/roboticsProjects.js
@@ -1,37 +1,45 @@
 const genericTemplate = require("../Utils/genericTemplate");
+const urlButton = require("../Utils/urlButton");
+
 const templateData = [{
-        title: "Yet to come",
-        subtitle: "click for more info",
-        image_url: "3a1957269552.ngrok.io/enquire.jpg",
+        title: "Quadruped Robot",
+        subtitle: "tap to show",
         buttons: [{
-            title: "More",
+            title: "show",
             payload: "projects_robotics_project1"
         }, ]
     },
     {
-        title: "Yet to come",
-        subtitle: "click for more info",
-        image_url: "3a1957269552.ngrok.io/enquire.jpg",
+        title: "NodeMCU hosted game",
+        subtitle: "tap to show",
         buttons: [{
-            title: "More",
+            title: "show",
             payload: "projects_robotics_project2"
         }, ]
     },
 ];
 
+const reply_url_data_1 = [{
+    url: "https://www.youtube.com/watch?v=NL_iKdTNOic&list=PLT1-rWM6m8PrjoYKDWJxAxmkRf11_wG6c",
+    title: "Open",
+    whRatio: "tall",
+}];
+
+const reply_url_data_2 = [{
+    url: "https://www.hackster.io/altahaaltaha/nodemcu-server-javascript-based-game-6b5741",
+    title: "Open",
+    whRatio: "tall",
+}];
+
 function roboticsProjects(payload) {
-    console.log(`webProjects.js:=> payload: ${payload}`);
+
     let response;
     switch (payload) {
         case "project1":
-            response = {
-                "text": "This is the info of my yet to come project"
-            };
+            response = urlButton(reply_url_data_1, "Tap on 'open' to get project info");
             break;
         case "project2":
-            response = {
-                "text": "This is the info of my yet to come project"
-            };
+            response = urlButton(reply_url_data_2, "Tap on 'open' to get project info");
             break;
 
         default:
