dbb143a2d3452e1c3c88c3d1f4a69e971e1ccae7
Altaha Ansari <altaha.altaha@gmail.com>
2020-06-22 19:08:01 +0530

    updated web projects info

--- Services/webProjects.js
+++ Services/webProjects.js
@@ -1,37 +1,45 @@
 const genericTemplate = require("../Utils/genericTemplate");
+const urlButton = require("../Utils/urlButton");
+
 const templateData = [{
         title: "Express-React Blog",
         subtitle: "click for more info",
-        image_url: "3a1957269552.ngrok.io/enquire.jpg",
         buttons: [{
-            title: "More",
+            title: "show",
             payload: "projects_web_blog"
         }, ]
     },
     {
         title: "Mess Management Web App",
-        subtitle: "click for more info",
-        image_url: "3a1957269552.ngrok.io/enquire.jpg",
+        subtitle: "Under development, will be updated soon",
         buttons: [{
-            title: "More",
+            title: "show",
             payload: "projects_web_mess"
         }, ]
     },
 ];
+
+const reply_url_data_1 = [{
+    url: "https://github.com/altaha-ansari/micronoob",
+    title: "Open",
+    whRatio: "tall",
+}];
+
+const reply_url_data_2 = [{
+    url: "https://github.com/altaha-ansari",
+    title: "Under Development",
+    whRatio: "tall",
+}];
 
 function webProjects(payload) {
 
     let response;
     switch (payload) {
         case "blog":
-            response = {
-                "text": "This is the info of my blog"
-            };
+            response = urlButton(reply_url_data_1, "Tap on 'open' to get project info");
             break;
         case "mess":
-            response = {
-                "text": "This is the info of my mess app"
-            };
+            response = urlButton(reply_url_data_2, "Button tap leads to my github profile");
             break;
 
         default:
